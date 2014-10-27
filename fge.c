0512d3d461b43932301c6b0c3b296f4c702e3614
Tyler Sanborn <tysanborn@gmail.com>
2014-10-26 20:49:39 -0400

    ffbhfhfhfhf

--- fge.c
+++ fge.c
@@ -1,37 +1,17 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <string.h>
 #include <openssl/blowfish.h>
 #include <openssl/crypto.h>
-
 #include <openssl/evp.h>
 
-int main(int argc, char **argv)
-{
-	EVP_CIPHER_CTX *ctx; /* SSL context */
-	EVP_CIPHER cipher; /* Cipher */
-	unsigned char key[EVP_MAX_KEY_LENGTH];
-	unsigned char msg[] = "Sample message.";
-	unsigned char * ciphertext;
-	char ivec[EVP_MAX_IV_LENGTH] = {0};
-	int mlen = strlen(msg) + 1;
-	int ctlen;
-	ctx = (EVP_CIPHER_CTX *) malloc(sizeof(EVP_CIPHER_CTX));
-	EVP_CIPHER_CTX_init(ctx);
-	cipher = EVP_bf_cbc(); 
-	/* Blowfish CBC mode
-	128-bit keys
-	(default size)*/
-	initialize_key(key);
-	/* Print key in HEX */
-	fprint_key(stdout,key,EVP_CIPHER_key_length(cipher));
-	/* Initialization context */
-	EVP_EncryptInit_ex(ctx, cipher, NULL, key, ivec);
-	/* Allocate space for the ciphertext */
-	ciphertext = allocate_ciphertext(mlen);
-	/* Encrypt data and print */
-	encrypt_and_print(ctx, msg, mlen, ciphertext,&ctlen, stdout);
-}
+#define MAXKEYLEN 56
+#define ACTKEYLEN 32
+#define BLOCKSIZE 8
+#define BUFSIZE 10
+
+
 void initialize_key(unsigned char *key)
 {
 	FILE *rng;
@@ -45,7 +25,7 @@ void initialize_key(unsigned char *key)
 unsigned char * allocate_ciphertext(int mlen)
 {
 	/* Alloc space for any possible padding. */
-	return (unsigned char *)malloc(mlen + BLOCKSIZE);
+	return (unsigned char *) malloc(mlen + BLOCKSIZE);
 }
 void encrypt_and_print(EVP_CIPHER_CTX * ectx, char *msg, int mlen,char *res, int *olen, FILE * f)
 {
@@ -60,4 +40,30 @@ void encrypt_and_print(EVP_CIPHER_CTX * ectx, char *msg, int mlen,char *res, int
 	fprint_string_as_hex(f, res, *olen);
 	fprintf(f, "\n");
 }
+int main(int argc, char **argv)
+{
+	EVP_CIPHER_CTX *ctx; /* SSL context */
+	const EVP_CIPHER *cipher = EVP_bf_cbc();/* Cipher */
+	unsigned char key[EVP_MAX_KEY_LENGTH];
+	unsigned char msg[] = "Sample message.";
+	unsigned char * ciphertext;
+	char ivec[EVP_MAX_IV_LENGTH] = {0};
+	int mlen = strlen(msg) + 1;
+	int ctlen;
+	ctx = (EVP_CIPHER_CTX *) malloc(sizeof(EVP_CIPHER_CTX));
+	EVP_CIPHER_CTX_init(ctx); 
+	/* Blowfish CBC mode
+	128-bit keys
+	(default size)*/
+	initialize_key(key);
+	/* Print key in HEX */
+	fprint_key(stdout,key,EVP_CIPHER_key_length(cipher));
+	/* Initialization context */
+	EVP_EncryptInit_ex(ctx, cipher, NULL, key, ivec);
+	/* Allocate space for the ciphertext */
+	ciphertext = allocate_ciphertext(mlen);
+	/* Encrypt data and print */
+	encrypt_and_print(ctx, msg, mlen, ciphertext,&ctlen, stdout);
+}
+
 
