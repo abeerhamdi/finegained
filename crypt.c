c55bf56daabd79ffdcf5c7f831ea0faf95ced565
James Hannah <jfhannah@mtu.edu>
2014-10-27 23:53:22 -0400

    .

--- crypt.c
+++ crypt.c
@@ -39,24 +39,43 @@ int encrypt(unsigned char *ptxt, unsigned char *key, unsigned char *ctxt){
 	int ctlen;
 	int extlen;
 	ctx = (EVP_CIPHER_CTX *) malloc(sizeof(EVP_CIPHER_CTX));
-	EVP_CIPHER_CTX_init(ctx); 
 	/* Blowfish CBC mode
 	128-bit keys
 	(default size)*/
 	/* Print key in HEX */
-	//fprint_string_as_hex(stdout,key,EVP_CIPHER_key_length(cipher));
+	EVP_CIPHER_CTX_init(ctx); 
 	/* Initialization context */
 	EVP_EncryptInit_ex(ctx, cipher, NULL, key, ivec);
 	/* Allocate space for the ciphertext */
 	ctxt = allocate_ciphertext(mlen);
-	/* Encrypt data and print */
-	//encrypt_and_print(ctx, msg, mlen, ciphertext,&ctlen, stdout);
 	EVP_EncryptUpdate(ctx, ctxt, &ctlen, ptxt, mlen);
 	EVP_EncryptFinal_ex(ctx, &ctxt[ctlen], &extlen);
+	free(ctx);
 	ctlen += extlen;
 	return ctlen;
 	//initialize_key(key);  Get random key for file
 	//unsigned char key[EVP_MAX_KEY_LENGTH];  Initilize key to size
+	//fprint_string_as_hex(stdout,key,EVP_CIPHER_key_length(cipher));
+}
+
+
+int decrypt(unsigned char *ptxt, unsigned char *key, unsigned char *ctxt, int ctlen){
+	int ptlen;
+	int extlen;
+	EVP_CIPHER_CTX *ctx; /* SSL context */
+
+	const EVP_CIPHER *cipher = EVP_bf_cbc();/* Cipher */
+	ctx = (EVP_CIPHER_CTX *) malloc(sizeof(EVP_CIPHER_CTX));
+	char ivec[EVP_MAX_IV_LENGTH] = {0};
+	ptxt = (unsigned char *) malloc(sizeof(char)*(ctlen - BLOCKSIZE));
+
+	EVP_CIPHER_CTX_init(ctx); 
+	EVP_DecryptInit_ex(ctx, cipher, NULL, key, ivec);
+	EVP_DecryptUpdate(ctx, ptxt, &ptlen, ctxt, ctlen);
+	EVP_DecryptFinal_ex(ctx, &ptxt[ptlen], &extlen);
+	ptlen += extlen;
+	free(ctx);
+	return ptlen;
 }
 
 void hash(unsigned char *keytext, unsigned char *sha){
--- fge.h
+++ fge.h
@@ -12,9 +12,11 @@
 #define ACTKEYLEN 32
 #define BLOCKSIZE 8
 #define BUFSIZE 10
+#define MAGIC 1420420420
 
 void fprint_string_as_hex(FILE * f, unsigned char *s, int len);
 void initialize_key(unsigned char *key);
 unsigned char * allocate_ciphertext(int mlen);
 int encrypt(unsigned char *ptxt, unsigned char *key, unsigned char *ctxt);
 void hash(unsigned char *keytext, unsigned char *sha);
+int decrypt(unsigned char *ptxt, unsigned char *key, unsigned char *ctxt, int ctlen);
